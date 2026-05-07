224743d9b337fe133988c1cc0835416f07f1dca0
Elysson Marconi <ellyssonmiike@hotmail.com>
2026-05-07 14:18:46 -0300

    refactor(feat/security-layer): refactored entity wrapping to instance loading

--- src/infra/domain/entities/user.entity.ts
+++ src/infra/domain/entities/user.entity.ts
@@ -1,5 +1,12 @@
+import { Type } from 'class-transformer';
 import { BaseEntity } from '@infra/base/entity';
 import { UserStatus } from '@database/enums';
+
+export class UserSessionEntity {
+  public accessToken: string;
+  public refreshToken: string;
+  public expiresAt?: Date;
+}
 
 export class UserEntity extends BaseEntity {
   public name: string;
@@ -7,15 +14,15 @@ export class UserEntity extends BaseEntity {
   public password: string;
   public status: UserStatus;
   public lastLoginAt: Date;
+
+  @Type(() => UserSessionEntity)
   public sessions: UserSessionEntity[];
 
   isActive() {
     return this.status === 'active';
   }
-}
 
-class UserSessionEntity {
-  public accessToken: string;
-  public refreshToken: string;
-  public expiresAt?: Date;
+  isSuspended() {
+    return this.status === 'suspended';
+  }
 }
